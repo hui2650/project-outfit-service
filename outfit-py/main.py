8d31d28961400fb29a1db7a637b007d21daed5f7
bichhuyen3108-max <bichhuyen3108@gmail.com>
2026-02-05 10:45:14 +0900

    Update main.py

--- outfit-py/main.py
+++ outfit-py/main.py
@@ -129,6 +129,7 @@ def color_compatible(user_color: str, cand_color: str) -> bool:
 
     return False
 
+
 # ================= BODY CHECK =================
 def bbox_fullbody_and_feet(img: Image.Image) -> Tuple[bool, Dict]:
     w, h = img.size
