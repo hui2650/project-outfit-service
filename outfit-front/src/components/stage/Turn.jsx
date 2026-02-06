24416d3a21380de0c1e7086af2f84bf1b9c734a2
taehui <hth4236727@gmail.com>
2026-02-06 18:12:05 +0900

    로딩상태에 이모지효과 추가

--- outfit-front/src/components/stage/Turn.jsx
+++ outfit-front/src/components/stage/Turn.jsx
@@ -2,19 +2,53 @@ import UserInputPreview from "./UserInputPreview";
 import ResultCarousel from "./ResultCarousel";
 
 const Turn = ({ turn, loading }) => {
-  return (
-    <div className="space-y-6">
-      {/* 유저 입력 */}
-      <UserInputPreview
-        previewUrl={turn.input.previewUrl}
-        textQuery={turn.input.textQuery}
-      />
+  const isLoadingOnly = loading && !turn.output;
+  const count = 30;
+  const emojiList = ["👗", "👜", "👠", "👒", "🧥"];
 
-      {/* 결과 */}
+  return (
+    <div
+      className={`${isLoadingOnly ? "h-screen flex flex-col items-center justify-center" : ""}`}
+    >
       {turn.output ? (
-        <ResultCarousel items={turn.output.items} loading={false} />
+        <div className="relative z-20">
+          {/* 결과 */}
+          <UserInputPreview
+            previewUrl={turn.input.previewUrl}
+            textQuery={turn.input.textQuery}
+          />
+          <ResultCarousel items={turn.output.items} loading={false} />
+        </div>
       ) : (
-        <div className="text-gray-400 text-sm">추천 생성 중...</div>
+        // 생성중
+        <div className="h-full flex items-center justify-center text-gray-400 text-sm">
+          추천 생성 중...
+        </div>
+      )}
+      {isLoadingOnly && (
+        <div className="absolute inset-0 overflow-hidden pointer-events-none z-10">
+          {[...Array(30)].map((_, i) => {
+            // 등간격을 기본으로 잡되, 약간의 무작위로 흔들어줌
+            const base = (i / count) * 100;
+            const offset = (Math.random() - 0.5) * (100 / count) * 0.7; // 흔들기 정도
+            const left = base + offset;
+
+            return (
+              <span
+                key={i}
+                className="absolute text-2xl animate-emojiFall"
+                style={{
+                  top: `${Math.random() * -100}vh`, // -20vh ~ 0vh 사이에서 시작
+                  left: `${left}%`,
+                  animationDelay: `${Math.random() * 1}s`,
+                  animationDuration: `${3 + Math.random() * 2}s`,
+                }}
+              >
+                {emojiList[i % emojiList.length]}
+              </span>
+            );
+          })}
+        </div>
       )}
     </div>
   );
