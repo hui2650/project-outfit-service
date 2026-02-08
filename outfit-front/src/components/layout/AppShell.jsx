c884ee6efcd58af34fd85109d36e459c39a2f923
taehui <hth4236727@gmail.com>
2026-02-08 23:20:57 +0900

    레이아웃 수정 header, appshell

--- outfit-front/src/components/layout/AppShell.jsx
+++ outfit-front/src/components/layout/AppShell.jsx
@@ -1,17 +1,17 @@
 // 레이아웃만 2컬럼 그리드
 
-import React from "react";
+import React from 'react'
 
 const AppShell = ({ left, right }) => {
   return (
-    <div className=" h-screen flex justify-between relative mx-auto bg-purple-50 shadow">
+    <div className=" h-screen flex justify-between relative mx-auto shadow">
       {/* 메인 2패널 */}
       <div className="flex-1 flex justify-center items-center overflow-hidden relative">
         {left}
       </div>
-      <div className="h-full bg-white">{right}</div>
+      <div className="h-full">{right}</div>
     </div>
-  );
-};
+  )
+}
 
-export default AppShell;
+export default AppShell
